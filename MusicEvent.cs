068bd146e99d698504e2c5cafe709ee79a854d45
soniaCastillo <49174035+soniaCastillo@users.noreply.github.com>
2023-12-12 06:11:57 -0500

    Change name classAgregar archivos de proyecto.

---
+++ MusicEvent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventHandler
+{
+    public  class MusicEvent
+    {
+        //  listener - signature must match event and delegate
+        public void RegisterEvent(object sender, EventInfo e)
+        {
+            Console.WriteLine("There is a music concert in " + e.Address);
+        }
+    }
+}
