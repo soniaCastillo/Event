3d0df41abd8c563c88621ed953953c4cd2c906f0
soniaCastillo <49174035+soniaCastillo@users.noreply.github.com>
2023-12-12 06:11:57 -0500

    Agregar archivos de proyecto.

---
+++ EventInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventHandler
+{
+    public class EventInfo : EventArgs
+    {
+        public string Address { get; set; }
+        public EventInfo(string address)
+        {
+            this.Address = address;
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿
+namespace EventHandler
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the concert event");
+
+            //Creating instances of the classes to subscribe and to raise the event in the publisher
+            MusicEvent music = new MusicEvent();
+
+            EventMonitoringCenter monitoringCenter = new EventMonitoringCenter();
+
+            //Subscribing to the events
+            monitoringCenter.CallForRegister += music.RegisterEvent;
+                                 
+            var address = "3456, Olohue Street E, Mississauga, ON L5B 4E8";
+
+            //Initiating the process
+            monitoringCenter.OnEventCallReceived(address);
+
+            Console.WriteLine("End of processing the register for concert event");
+
+        }
+
+    }
+}
